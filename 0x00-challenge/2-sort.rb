b21aff4935bc956d4526a05d0a664a3ba2879c58
Robinson Montes <1574@holbertonschool.com>
2020-08-20 21:47:10 +0000

    sorting debugging again

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -17,7 +17,7 @@ ARGV.each do |arg|
     i = 0
     l = result.size
     while !is_inserted && i < l do
-        if result[i] > i_arg
+        if result[i] < i_arg
             i += 1
         else
             result.insert(i, i_arg)
